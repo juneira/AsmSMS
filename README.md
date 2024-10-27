f0e0d0af438baaf04caccb3b0441cd993327bc25
Marcelo Junior <marcelo.jr63@gmail.com>
2024-10-27 11:42:08 -0300

    Initial commit :tada:

---
+++ README.md
@@ -0,0 +1,2 @@
+# ASM - SMS
+### A study about Z80 and SMS (Sega Mater System)
---
+++ playground.asm
@@ -0,0 +1,42 @@
+org 0x0000
+
+di
+
+; initialize the program
+call init_color
+
+start:
+  ld a, 50h
+  jp start
+
+; function init_color
+; initialize CRAM
+init_color:
+  ld hl, color
+  ld b, 4
+
+  init_color_loop:
+    ld a, 4
+    sub b
+
+    ; set CRAM Address
+    out (0bfh), a
+    ld a, 0c0h
+    out (0bfh), a
+
+    ; set CRAM Data
+    ld a, (hl)
+    out (0beh), a
+
+    inc hl
+
+    djnz init_color_loop
+  ret
+
+; COLOR CRAM
+color:
+  db 0b00110000
+  db 0b00001100
+  db 0b00000011
+  db 0b00000111
+color_end:
